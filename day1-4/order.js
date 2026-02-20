69ad83ff8ed7ff8ec279403eba7b5585b21c5cc5
sairampv5 <ramsaipv5@gmail.com>
2026-02-20 15:54:09 +0530

    Day5: Backend Structure

--- day1-4/order.js
+++ day1-4/order.js
@@ -9,3 +9,4 @@ const totalBill = customerOrder.quantity * customerOrder.pricePerUnit;
 console.log("Customer: " + customerOrder.customerName);
 console.log("Ordered: " + customerOrder.item);
 console.log("Total Amount: " + totalBill);
+console.log(customerOrder);
